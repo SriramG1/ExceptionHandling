c6672568744599f885cec04ea095e275ab5a9a46
SriramG1 <sriramhht@gmail.com>
2022-02-12 08:51:13 +0530

    'Number format exception'

--- src/com/company/NumberFormat.java
+++ src/com/company/NumberFormat.java
@@ -13,11 +13,11 @@ public class NumberFormat {
         System.out.println("Your string value is "+text);
         System.out.println("Enter Your integer :");
         try {
-        int number = Integer.parseInt(br.readLine());
-        System.out.println("Your integer value is :" + number);
+            int number = Integer.parseInt(br.readLine());
+            System.out.println("Your integer value is :" + number);
         }
         catch (NumberFormatException nfe){
-        System.err.println("Enter only integers !");
+            System.err.println("Enter only integers !");
         }
         System.out.println("Enter the float :");
         float floatValue= Float.parseFloat(br.readLine());
