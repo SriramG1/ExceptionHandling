a145a81cddcbe270ec54e1f18e118d3d8093b6f8
SriramG1 <sriramhht@gmail.com>
2022-02-13 09:36:20 +0530

    'Example for checked Exception'

---
+++ src/com/company/CheckedException.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CheckedException {
+    public static void main(String []args) throws IOException
+    {
+        try {
+            FileInputStream fis = new FileInputStream("B:/myfile.txt");
+            int k;
+
+            while ((k = fis.read()) != -1) {
+                System.out.print((char) k);
+            }
+            fis.close();
+        }
+        catch(IOException ioException){
+            System.err.println("Your file is not founded!!!");
+        }
+    }
+}
