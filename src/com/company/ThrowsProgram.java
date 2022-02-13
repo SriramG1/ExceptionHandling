a7cef657d0c3ac404a346589fc8f78b4be659a06
SriramG1 <sriramhht@gmail.com>
2022-02-13 08:54:50 +0530

    'Add another input mismatch exception in this program'

--- src/com/company/ThrowsProgram.java
+++ src/com/company/ThrowsProgram.java
@@ -1,21 +1,30 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
     class ThrowsDemo {
-        public void Division(int numberOne,int numberTwo) throws ArithmeticException{
+        public void Division(int numberOne,int numberTwo) throws ArithmeticException, InputMismatchException {
            int result=numberOne/numberTwo;
             System.out.println("Your division value is "+result);
         }
     }
     public class ThrowsProgram  {
         public static void main(String[] args) {
-            ThrowsDemo obj=new ThrowsDemo();
+            Scanner in= new Scanner(System.in);
             try {
-                obj.Division(10, 0);
+            System.out.println("Enter  Your First Number :");
+            int firstNumber=in.nextInt();
+            System.out.println("Enter  Your Second Number :");
+            int secondNumber=in.nextInt();
+            ThrowsDemo obj=new ThrowsDemo();
+                obj.Division(firstNumber, secondNumber);
             }
-            catch (ArithmeticException ae){
-                System.out.println("Don't divide by zero");
+            catch (ArithmeticException arithmeticException){
+                System.err.println("Don't divide by zero");
+            }
+            catch (InputMismatchException inputMismatchException){
+                System.err.println("Enter valid Integers only");
             }
     }
 }
